4d388e330697d45fb48a8015a603eafe01bba526
alaakson <162610032+alaakson@users.noreply.github.com>
2024-08-02 11:45:32 +0300

    Create minitalk.h

---
+++ minitalk.h
@@ -0,0 +1,16 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "ft_printf/ft_printf.h"
+# include <signal.h>
+# include <signal.h>
+# include <errno.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *s);
+int	ft_atoi(const char *str);
+
+#endif
